257b4113b25cf03cc85b7ebb6398b97460944428
Joss Steward <Joss.Steward@gmail.com>
2015-03-23 10:55:02 -0400

    Now accepts (and expects) two command line args.
    The proper way to run this is now "./main 1 1000 +RTS -N" where 1 is the
    lower limit and 1000 is the upper limit. -N ensures it will
    automatically use as many threads as you computer has cores.
    Hyperthreaded "cores" count in this case and your performance may
    decrease if more threads are spawned than there are physical cores.

--- main.hs
+++ main.hs
@@ -1,8 +1,10 @@
 import Primes
-import Control.Monad.Par
 import Control.Monad
-import Data.List.Split
+import Control.Monad.Par
 import Control.Concurrent
+import Data.List
+import Data.List.Split
+import System.Environment
 
 processChunk :: [Integer] -> [Integer]
 processChunk a = filter isPrime a
@@ -11,6 +13,20 @@ findPrimes :: [Integer] -> Int -> [[Integer]]
 findPrimes a n = runPar $ parMap processChunk $ splitEvery ((length a) `div` n) a
 
 main = do
+   args <- getArgs
    n <-  getNumCapabilities
-   print $ findPrimes [1..1000000] n
-   -- print $ calculatePrimes 1 500000 1000000
+   case args of
+      [lowerStr, upperStr] -> do
+         putStrLn $ "Calculating Primes from " ++ lowerStr ++ " to " ++ upperStr
+         putStrLn $ "Using " ++ show n ++ " threads"
+         putStrLn $ show $ findPrimes [lower..upper] n
+         where
+            lower = read lowerStr :: Integer
+            upper = read upperStr :: Integer
+         -- print $ "lower: " ++ lower ++ " upper: " ++ upper
+      _ ->
+         print "Not gonna work"
+         -- print $ findPrimes [1..1000000] n
+
+
+
