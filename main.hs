a40f80b91687fe83821e75f4e35af8b8acafd98e
ChrisHersh <ch5968@cs.ship.edu>
2015-03-23 08:54:14 -0400

    The range is now evenly divided between the threads

--- main.hs
+++ main.hs
@@ -2,12 +2,15 @@ import Primes
 import Control.Monad.Par
 import Control.Monad
 import Data.List.Split
+import Control.Concurrent
 
 processChunk :: [Integer] -> [Integer]
 processChunk a = filter isPrime a
 
-findPrimes :: [Integer] -> [[Integer]]
-findPrimes a = runPar $ parMap processChunk $ splitEvery 500000 a
+findPrimes :: [Integer] -> Int -> [[Integer]]
+findPrimes a n = runPar $ parMap processChunk $ splitEvery ((length a) `div` n) a
 
-main = print $ findPrimes [1..1000000]
+main = do
+   n <-  getNumCapabilities
+   print $ findPrimes [1..1000000] n
    -- print $ calculatePrimes 1 500000 1000000
