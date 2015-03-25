1584fef28e4c32ce34676d9274bd64d5d763558a
Joss Steward <Joss.Steward@gmail.com>
2015-03-24 22:36:59 -0400

    Implemented a simple demo of command line switches with getOpt. Note that this has broken the current version. It compiles fine, but will not run since that line is commented out. This was only done for convenience while testing.
    Also, getOpt is really freaking cool.

--- main.hs
+++ main.hs
@@ -5,28 +5,69 @@ import Control.Concurrent
 import Data.List
 import Data.List.Split
 import System.Environment
+import System.Console.GetOpt
+import System.IO
+import System.Exit
+import Data.Char
 
+-- Prime Mining --
 processChunk :: [Integer] -> [Integer]
 processChunk a = filter isPrime a
 
 findPrimes :: [Integer] -> Int -> [[Integer]]
 findPrimes a n = runPar $ parMap processChunk $ splitEvery ((length a) `div` n) a
 
+-- Command line option handling --
+data Options = Options { optControllerIP :: String }
+
+-- Default Options --
+startOptions :: Options
+startOptions = Options { optControllerIP = "127.0.0.1" }
+
+options :: [ OptDescr (Options -> IO Options) ]
+options =
+   [ Option "s" ["server"]
+      (ReqArg
+         (\arg opt -> return opt { optControllerIP = arg })
+         "SERVER IP")
+      "IP of the Control Server"
+   , Option "h" ["help"]
+      (NoArg
+         (\_ -> do
+            prg <- getProgName
+            hPutStrLn stderr (usageInfo prg options)
+            exitWith ExitSuccess))
+      "Show help"
+   ]
+
 main = do
+   n <- getNumCapabilities
    args <- getArgs
-   n <-  getNumCapabilities
+
+   let (actions, nonOptions, errors) = getOpt RequireOrder options args
+   opts <- foldl (>>=) (return startOptions) actions
+
+   let Options { optControllerIP = serverIp } = opts
+   putStrLn $ "Server IP: " ++ serverIp
+
    case args of
       [lowerStr, upperStr] -> do
          putStrLn $ "Calculating Primes from " ++ lowerStr ++ " to " ++ upperStr
          putStrLn $ "Using " ++ show n ++ " threads"
-         putStrLn $ show $ findPrimes [lower..upper] n
+         --putStrLn $ show $ findPrimes [lower..upper] n
          where
             lower = read lowerStr :: Integer
             upper = read upperStr :: Integer
-         -- print $ "lower: " ++ lower ++ " upper: " ++ upper
       _ ->
          print "Not gonna work"
-         -- print $ findPrimes [1..1000000] n
+
+
+
+
+
+
+
+
 
 
 
