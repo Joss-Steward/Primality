cdf0411fc9840de15d9da0317ed9f21d66af78cf
ChrisHersh <ch5968@cs.ship.edu>
2015-03-25 16:02:54 -0400

    The server now works, does not maintain connection

--- serverTest.hs
+++ serverTest.hs
@@ -13,32 +13,34 @@ import Network
 -- findPrimes :: [Integer] -> Int -> [[Integer]]
 -- findPrimes a n = runPar $ parMap processChunk $ splitEvery ((length a) `div` n) a
 
+blockSize :: Int
 blockSize = 1000000
 
 startServer = withSocketsDo $ do
     sock <- listenOn  $ PortNumber 32101
     
-    sockHandler sock [1, blockSize .. ]
+    sockHandler sock list
+    where
+        list :: [Int]
+        list = [1, blockSize .. ]
     
 
-sockHandler :: Socket -> IO ()
+sockHandler :: Socket -> [Int] -> IO ()
 sockHandler sock list = do
     (handle, _, _) <- accept sock
     hSetBuffering handle NoBuffering
     x <- hGetLine handle
     
-    forkIO $ handleRequest handle $ fst list
-    drop x list
+    forkIO $ handleRequest handle (list !! 0) (list !! (read x))
     
-    
-    sockHandler sock
+    sockHandler sock $ drop (read x) list
 
-handleRequest handle r = do
-    hPutStrLn handle lower upper
+
+handleRequest handle l u = do
+    hPutStrLn handle (lower ++ " " ++ upper)
     where
-        strRange = show r
-        lower = r
-        upper = show (r + blockSize)
+        lower = show l
+        upper = show u
     
     
     
