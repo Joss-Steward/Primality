f6d0d84f55dfb7786e111be19906326697d059d6
ChrisHersh <ch5968@cs.ship.edu>
2015-03-30 09:37:43 -0400

    Changed chan to MVar, fixing the memory leak

--- logger.hs
+++ logger.hs
@@ -2,7 +2,7 @@ import Network
 import System.IO
 import Control.Exception
 import Control.Concurrent
-import Control.Concurrent.Chan
+import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.Fix (fix)
 
@@ -12,21 +12,20 @@ filename = "primes.log"
 
 main :: IO ()
 main = do
-   chan <- newChan
+   chan <- newEmptyMVar
 --   sock <- socket AF_INET Stream 0
 --   setSocketOption sock ReuseAddr 1
 --   bindSocket sock (SockAddrInet 4242 iNADDR_ANY)
 --   listen sock 2
    sock <- listenOn $ PortNumber 4242 
-   chan' <- dupChan chan
    reader <- forkIO $ fix $ \loop -> do
-      (nr', line) <- readChan chan'
+      (nr', line) <- takeMVar chan
       putStrLn $ "Results returned from slave " ++ show nr'
       appendFile filename (line ++ "\n")
       loop
    mainLoop sock chan 0
 
-mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+mainLoop :: Socket -> MVar (Msg) -> Int -> IO ()
 mainLoop sock chan nr = do
    (handle, _, _) <- accept sock
    hSetBuffering handle NoBuffering
@@ -34,9 +33,9 @@ mainLoop sock chan nr = do
    forkIO  $ runConn handle chan nr
    mainLoop sock chan $! nr+1
 
-runConn :: Handle -> Chan Msg -> Int -> IO ()
+runConn :: Handle -> MVar (Msg) -> Int -> IO ()
 runConn hdl chan nr = do
-   let broadcast msg = writeChan chan (nr, msg)
+   let broadcast msg = putMVar chan (nr, msg)
    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
       line <- hGetLine hdl
       broadcast line
